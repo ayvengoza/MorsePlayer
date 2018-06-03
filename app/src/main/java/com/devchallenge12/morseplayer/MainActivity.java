af42161accba671f9f476e4a40d2d62add10d8ae
andriizastupailo <ayvengoza@gmail.com>
2018-06-03 17:51:48 +0300

    add requirements and refactor

--- app/src/main/java/com/devchallenge12/morseplayer/MainActivity.java
+++ app/src/main/java/com/devchallenge12/morseplayer/MainActivity.java
@@ -3,8 +3,9 @@ package com.devchallenge12.morseplayer;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
+import android.media.PlaybackParams;
+import android.os.Build;
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -25,11 +26,12 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_CODE = 0;
-    private static final int PROGRESS_DEVIDER = 10;
+    private static final int PROGRESS_UPDATE = 10;
+    private static final float[] AUDIO_SPEEDS = new float[]{0.5f, 0.75f, 1.0f, 1.25f, 1.5f, 2.0f};
 
     private EditText mMorseInput;
     private TextView mMorseOutput;
-    private SeekBar mSeekProgress;
+    private SeekBar mSeekPosition;
     private TextView mSpeedStatus;
     private SeekBar mSeekSpeed;
     private Button mPlayPause;
@@ -38,10 +40,19 @@ public class MainActivity extends AppCompatActivity {
     private Handler mHandler;
     private Runnable mRunnable;
 
+    private int mPosition = 0;
+    private int mSpeed = 2;
+    private boolean isAudioRelevant = false;
+    private boolean isPalying = false;
+
     private View.OnClickListener mOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            startPlayer();
+            if(isPalying){
+                pauseAction();
+            } else {
+                playAction();
+            }
         }
     };
 
@@ -53,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
+            isAudioRelevant = false;
+            setSeekPositionProgress(0);
             setMorseOutputText(MorseConvertor.textToMorseMsg(s.toString()));
         }
 
@@ -66,8 +79,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
             switch (seekBar.getId()){
-                case R.id.sbSeekProgress:
-                    setAudioProgress(progress, fromUser);
+                case R.id.sbSeekPosition:
+                    setAudioPosition(progress, fromUser);
+                    break;
+                case R.id.sbSeekSpeed:
+                    setAudioSpeed(progress, fromUser);
                     break;
             }
         }
@@ -83,13 +99,6 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private void setAudioProgress(int progress, boolean fromUser) {
-        if(mMediaPlayer != null && fromUser){
-            mMediaPlayer.seekTo(progress * PROGRESS_DEVIDER);
-        }
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -100,8 +109,8 @@ public class MainActivity extends AppCompatActivity {
 
         mMorseOutput = (TextView) findViewById(R.id.tvMorseOutput);
 
-        mSeekProgress = (SeekBar) findViewById(R.id.sbSeekProgress);
-        mSeekProgress.setOnSeekBarChangeListener(mOnSeekBarChangeListener);
+        mSeekPosition = (SeekBar) findViewById(R.id.sbSeekPosition);
+        mSeekPosition.setOnSeekBarChangeListener(mOnSeekBarChangeListener);
 
         mSpeedStatus = (TextView) findViewById(R.id.tvSpeedStatus);
 
@@ -112,9 +121,23 @@ public class MainActivity extends AppCompatActivity {
         mPlayPause.setOnClickListener(mOnClickListener);
 
         mHandler = new Handler();
+        setSpeedStatus(mSpeed);
     }
 
-    public void startPlayer(){
+    public void setMorseOutputText(String outputText){
+        mMorseOutput.setText(outputText);
+    }
+
+    private void playAction(){
+        if(isAudioRelevant){
+            play();
+        } else {
+            createPlayer();
+            play();
+        }
+    }
+
+    public void createPlayer(){
         String inputText = mMorseInput.getText().toString();
         String filePath = "/sdcard/morse.midi";
         FileDescriptor fd = null;
@@ -127,14 +150,25 @@ public class MainActivity extends AppCompatActivity {
 
                 if(mMediaPlayer == null){
                     mMediaPlayer = new MediaPlayer();
+                    mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            endAction();
+                        }
+                    });
+                    mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            setAudioPosition(0, true);
+                        }
+                    });
                 } else {
                     mMediaPlayer.reset();
                 }
 
                 mMediaPlayer.setDataSource(fd);
                 mMediaPlayer.prepare();
-                mMediaPlayer.start();
-                initializeSeekBar();
+                isAudioRelevant = true;
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
@@ -143,12 +177,128 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void updateProgress(int progress) {
-        mSeekSpeed.setProgress(progress);
+    private void play(){
+        if(mMediaPlayer != null){
+            mMediaPlayer.start();
+            setAudioPosition(mPosition, true);
+            setAudioSpeed(mSpeed, true);
+            initializeSeekBar();
+            if(mMediaPlayer.isPlaying()){
+                mPlayPause.setText(R.string.btn_pause);
+                isPalying = true;
+            }
+        }
     }
 
-    public void setMorseOutputText(String outputText){
-        mMorseOutput.setText(outputText);
+    private void pauseAction(){
+        storeCurrentPosition();
+        pause();
+    }
+
+    private void pause(){
+        if(mMediaPlayer != null){
+            mMediaPlayer.pause();
+            if(!mMediaPlayer.isPlaying()){
+                isPalying = false;
+                mPlayPause.setText(R.string.btn_play);
+            }
+        }
+        cleanHandler();
+    }
+
+    private void endAction(){
+        pause();
+        setAudioPosition(0, true);
+    }
+
+    private void stopAction(){
+        if(mMediaPlayer != null){
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+            cleanHandler();
+        }
+    }
+
+    private void cleanHandler(){
+        if(mHandler != null){
+            mHandler.removeCallbacks(mRunnable);
+        }
+    }
+
+    private void initializeSeekBar(){
+        mSeekPosition.setMax(mMediaPlayer.getDuration());
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                updatePosition();
+                mHandler.postDelayed(mRunnable, PROGRESS_UPDATE);
+            }
+        };
+        mHandler.postDelayed(mRunnable, PROGRESS_UPDATE);
+    }
+
+    public void updatePosition() {
+        if (mMediaPlayer != null) {
+            storeCurrentPosition();
+            setSeekPositionProgress(getPosition());
+        }
+    }
+
+    private void storeCurrentPosition(){
+        if(mMediaPlayer != null){
+            int duration = mMediaPlayer.getDuration();
+            int currentPosition = mMediaPlayer.getCurrentPosition();
+            if (currentPosition != duration){
+                setPosition(currentPosition);
+                setSeekPositionProgress(getPosition());
+            } else {
+                endAction();
+            }
+        } else {
+            setPosition(0);
+        }
+    }
+
+    private void setAudioSpeed(int progress, boolean fromUser) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (mMediaPlayer != null && fromUser) {
+                if (mMediaPlayer.isPlaying()) {
+                    PlaybackParams params = mMediaPlayer.getPlaybackParams();
+                    params.setSpeed(AUDIO_SPEEDS[progress % AUDIO_SPEEDS.length]);
+                    mMediaPlayer.setPlaybackParams(params);
+                }
+            }
+            setSpeedStatus(progress);
+        } else {
+            // Alert needed verssion of Android
+        }
+    }
+
+    private void setAudioPosition(int progress, boolean fromUser) {
+        setSeekPositionProgress(progress);
+        if(mMediaPlayer != null && fromUser){
+            mMediaPlayer.seekTo(getPosition());
+        }
+    }
+
+    private void setPosition(int position){
+        mPosition = position;
+    }
+
+    private int getPosition(){
+        return mPosition;
+    }
+
+    private void setSeekPositionProgress(int position){
+        setPosition(position);
+        mSeekPosition.setProgress(getPosition());
+    }
+
+    private void setSpeedStatus(int index){
+        mSpeed = index;
+        String status = "x" + AUDIO_SPEEDS[mSpeed];
+        mSpeedStatus.setText(status);
     }
 
     private boolean checkPermission(){
@@ -173,36 +323,9 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-    private void initializeSeekBar(){
-        mSeekProgress.setMax(mMediaPlayer.getDuration()/PROGRESS_DEVIDER);
-
-        mRunnable = new Runnable() {
-            @Override
-            public void run() {
-                if(mMediaPlayer!= null){
-                    int mCurrentPosition = mMediaPlayer.getCurrentPosition()/PROGRESS_DEVIDER;
-                    mSeekProgress.setProgress(mCurrentPosition);
-                }
-                mHandler.postDelayed(mRunnable, PROGRESS_DEVIDER);
-            }
-        };
-        mHandler.postDelayed(mRunnable, PROGRESS_DEVIDER);
-    }
-
-    private void stopPlaying(){
-        if(mMediaPlayer != null){
-            mMediaPlayer.stop();
-            mMediaPlayer.release();
-            mMediaPlayer = null;
-            if(mHandler != null){
-                mHandler.removeCallbacks(mRunnable);
-            }
-        }
-    }
-
     @Override
     protected void onStop() {
         super.onStop();
-        stopPlaying();
+        stopAction();
     }
 }
